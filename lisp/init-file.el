d37e5d20e6339e320dd85cc604555ece765f004b
阎先生 <noalias@petalmail.com>
2023-10-11 12:40:44 +0800

    Add file command prefix.

--- lisp/init-file.el
+++ lisp/init-file.el
@@ -7,17 +7,22 @@
         create-lockfiles nil
         auto-save-delete-trailing-whitespace t
         delete-auto-save-files t)
+  (defvar-keymap file:command-map
+    "l" #'recentf
+    "r" #'rename-file
+    "c" #'copy-file
+    "k" #'delete-file)
+  (fset 'file:command-map file:command-map)
   :hook (after-init-hook . recentf-mode)
   :bind
-  ("C-x f" . recentf)
+  ("C-c f" . file:command-map)
   :custom
   (recentf-exclude `(,no-littering-var-directory
                      ,no-littering-etc-directory
                      "Scoop"))
   :config
   ;; Active 'find file at point'.
-  (ffap-bindings)
-  )
+  (ffap-bindings))
 
 (provide 'init-file)
 ;;; init-file.el ends here
